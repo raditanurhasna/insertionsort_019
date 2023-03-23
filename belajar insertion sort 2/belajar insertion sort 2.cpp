1e9c764c4613d2c1ddfa0bd7e60ff8849813368c
raditanurhasna <raditanurhasna1410@gmail.com>
2023-03-23 10:51:06 +0700

    Menambahkan prosedur Pemprograman

--- belajar insertion sort 2/belajar insertion sort 2.cpp
+++ belajar insertion sort 2/belajar insertion sort 2.cpp
@@ -4,26 +4,29 @@ using namespace std;
 int arr[20];
 int n;
 
-void input() {					// Procedure Input
+void input() {					
 	while (true)
 	{
-		cout << "Masukan Jumlah Data pada Array : ";	// Membuat Inputan jumlah element Array
-		cin >> n;										// Membuat variabel Inputan n 
+		cout << "Masukan Jumlah Data pada Array : ";	
+		cin >> n;										
 
-		if (n <= 20) {									// Membuat kondisi n tidak lebih dari 20
+		if (n <= 20) {									
 			break;
 		}
 		else;
 		{
-			cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";	// Menampilkan Pesan jika data lebih dari 20
+			cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";	
 		}
 	}
 	cout << endl;
-	cout << "=====================" << endl;			// Membuat jarak per baris program
-	cout << "Masukan Element Array" << endl;			// Membuat tampilan susunan data element array
+	cout << "=====================" << endl;			
+	cout << "Masukan Element Array" << endl;			
 	cout << "=====================" << endl;
 
-	for (int i = 0; i < n; i++)	
-	{ 
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data ke-" << (i + 1) << "; ";
+		cin >> arr[i];
+	}
 }
 
