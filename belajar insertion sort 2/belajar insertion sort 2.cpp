736900ca59afb734ea773f5e33936ddf9ed7873f
raditanurhasna <raditanurhasna1410@gmail.com>
2023-03-23 11:11:29 +0700

    Membuat Display untuk Array yang sudah disusun menggunakan algoritma insertionsort.

--- belajar insertion sort 2/belajar insertion sort 2.cpp
+++ belajar insertion sort 2/belajar insertion sort 2.cpp
@@ -56,3 +56,20 @@ void insertionsort() {
 		}
 	}
 }
+
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element array yang telah tersusun" << endl;
+	cout << "=================================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
+}
+int main() {
+	input();
+	insertionsort();
+	display();
+	return 0;
+}
